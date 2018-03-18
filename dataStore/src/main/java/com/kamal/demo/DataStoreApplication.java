741b92276c9fc296fb9970b38b21624667bf17b9
kamal <kamalprabakaran@gmail.com>
2018-03-18 23:57:22 +0530

    Initial commit

---
+++ dataStore/src/main/java/com/kamal/demo/DataStoreApplication.java
@@ -0,0 +1,12 @@
+package com.kamal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStoreApplication.class, args);
+	}
+}
---
+++ spring-boot-2-jdbc-with-h2/src/main/java/com/jdbc/h2/SpringBoot2JdbcWithH2Application.java
@@ -0,0 +1,30 @@
+package com.jdbc.h2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JdbcWithH2Application  {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
+	}
+
+	/*@Override
+	public void run(String... args) throws Exception {
+
+		//logger.info("Student id 10001 -> {}", repository.findById(10001L));
+
+		//logger.info("Inserting -> {}", repository.insert(new Student(10010L, "John", "A1234657")));
+
+		//logger.info("Update 10003 -> {}", repository.update(new Student(10001L, "Name-Updated", "New-Passport")));
+
+		//repository.deleteById(10002L);
+
+		//logger.info("All users -> {}", repository.findAll());
+	}*/
+}
